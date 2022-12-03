2b3ee4dca86935fd97ffe077c646f0ed61502ee0
Sergey Shevchenko <sergiishevchenko017@gmail.com>
2022-12-03 18:10:42 +0100

    refactored ifmain & try-except block

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import requests
 import telegram
 import time
 
-from requests.exceptions import ConnectionError
+from requests.exceptions import ConnectionError, ReadTimeout
 
 from dotenv import load_dotenv
 
@@ -12,35 +12,41 @@ logger = logging.getLogger(__name__)
 
 
 def main():
-    url = 'https://dvmn.org/api/user_reviews/'
-    headers = {
-        'Authorization': 'Token {}'.format(os.getenv('DEVMAN_TOKEN'))
-    }
-    response = requests.get(url, headers=headers)
-    response.raise_for_status()
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+
+    load_dotenv()
+    bot = telegram.Bot(token=os.getenv('TELEGRAM_TOKEN'))
+
     while True:
         try:
-            long_polling_url = 'https://dvmn.org/api/long_polling/'
-            requests.get(long_polling_url, headers=headers)
+            url = 'https://dvmn.org/api/long_polling/'
+            headers = {
+                'Authorization': 'Token {}'.format(os.getenv('DEVMAN_TOKEN'))
+            }
+            response = requests.get(url, headers=headers)
+            response.raise_for_status()
 
-            results = response.json().get('results', [])
-            last_record = results[0]
-            if last_record.get('is_negative'):
-                text = "У вас проверили работу '{}'.\n\n К сожалению, в работе нашлись ошибки.\n\n Ссылка на урок - {}"\
-                    .format(last_record.get('lesson_title'), last_record.get('lesson_url'))
-            else:
-                text = "У вас проверили работу '{}'.\n\n Преподавателю всё понравилось, можно приступать к следующему уроку!\n\n Ссылка на урок - {}"\
-                    .format(last_record.get('lesson_title'), last_record.get('lesson_url'))
-            bot.send_message(chat_id=os.getenv('TG_CHAT_ID'), text=text)
-        except ConnectionError as error:
+            if response.json().get('status') == 'timeout':
+                timestamp_to_request = response.json().get('timestamp_to_request', '')
+                response = requests.get(url, headers=headers, params={'timestamp': timestamp_to_request})
+                response.raise_for_status()
+
+            if response.json().get('new_attempts'):
+                new_attempts = response.json().get('new_attempts', [])
+                last_record = new_attempts[0]
+                if last_record.get('is_negative'):
+                    text = "У вас проверили работу '{}'.\n\n К сожалению, в работе нашлись ошибки.\n\n Ссылка на урок - {}"\
+                        .format(last_record.get('lesson_title'), last_record.get('lesson_url'))
+                else:
+                    text = "У вас проверили работу '{}'.\n\n Преподавателю всё понравилось, можно приступать к следующему уроку!\n\n Ссылка на урок - {}"\
+                        .format(last_record.get('lesson_title'), last_record.get('lesson_url'))
+                bot.send_message(chat_id=os.getenv('TG_CHAT_ID'), text=text)
+        except (ReadTimeout, ConnectionError) as error:
+            time.sleep(60)
             return 'Exception description - {}'.format(error)
-        time.sleep(60)
 
 
 if __name__ == '__main__':
-    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
-    load_dotenv()
-    bot = telegram.Bot(token=os.getenv('TELEGRAM_TOKEN'))
     main()
 
 
