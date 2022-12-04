6cff9cf9c1cbf42ad1a176f52cc48461bd6ea843
Sergey Shevchenko <sergiishevchenko017@gmail.com>
2022-12-04 17:49:08 +0100

    return -> print & fixed .json()

--- main.py
+++ main.py
@@ -27,14 +27,15 @@ def main():
             }
             response = requests.get(url, headers=headers, params={'timestamp': timestamp_to_request})
             response.raise_for_status()
+            response = response.json()
 
-            if response.json().get('status') == 'timeout':
-                timestamp_to_request = response.json().get('timestamp_to_request', '')
-            if response.json().get('status') == 'found':
-                timestamp_to_request = response.json().get('last_attempt_timestamp', '')
+            if response.get('status') == 'timeout':
+                timestamp_to_request = response.get('timestamp_to_request', '')
+            if response.get('status') == 'found':
+                timestamp_to_request = response.get('last_attempt_timestamp', '')
 
-            if response.json().get('new_attempts'):
-                new_attempts = response.json().get('new_attempts', [])
+            if response.get('new_attempts'):
+                new_attempts = response.get('new_attempts', [])
                 last_record = new_attempts[0]
                 if last_record.get('is_negative'):
                     text = "У вас проверили работу '{}'.\n\n К сожалению, в работе нашлись ошибки.\n\n Ссылка на урок - {}"\
@@ -45,9 +46,9 @@ def main():
                 bot.send_message(chat_id=os.getenv('TG_CHAT_ID'), text=text)
         except ConnectionError as error:
             time.sleep(60)
-            return 'Exception description - {}'.format(error)
+            print('Exception description - {}'.format(error))
         except ReadTimeout as error:
-            return 'Exception description - {}'.format(error)
+            print('Exception description - {}'.format(error))
 
 
 if __name__ == '__main__':
